a622f982243f4721be90cc59cb84a8e9058cd18a
Shreyas Vijaykumar <shreyasv@gmail.com>
2014-12-05 21:21:28 -0500

    dynamic port for heroku

--- gcloud.py
+++ gcloud.py
@@ -1,8 +1,10 @@
 from bottle import route, run, template, request, redirect, static_file
+import os
 
 @route('/')
 def index():
 	return "Hello, World!!!"
 
-
-run(host='localhost', port=80)
+port = int(os.environ.get("PORT",8081))
+print 'PORT!!!!!!! %d' % (port)
+run(host='0.0.0.0', port=port)
