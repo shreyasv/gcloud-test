ad0c816320f3b1cd22e3fb4aab8178269ebe95ca
Shreyas Vijaykumar <shreyasv@gmail.com>
2014-12-06 10:42:18 -0500

    print foo to browser

--- gcloud.py
+++ gcloud.py
@@ -3,9 +3,11 @@ import os
 
 @route('/')
 def index():
-	return "Hello, World!!!"
+	print "Hello, World!!!"
+	print foo
+	return
 
 port = int(os.environ.get("PORT",8081))
-print 'PORT!!!!!!! %d' % (port)
-print os.environ.get("FOO", "Could Not Find FOO")
+print 'PORT %d' % (port)
+foo = os.environ.get("FOO", "Could Not Find FOO")
 run(host='0.0.0.0', port=port)
